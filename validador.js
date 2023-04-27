22a6e6e33bc3db15453facca2d14a49e9769c0a3
taynara-yt <taynarasilvaam@gmail.com>
2023-04-27 19:46:28 -0400

    inserção do arquivo validador.js

---
+++ validador.js
@@ -0,0 +1,31 @@
+function validarEmail(email) {
+    // Expressão regular para validar email
+    const padrao = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return padrao.test(email);
+  }
+  
+  function validarCPF(cpf) {
+    // Expressão regular para validar CPF
+    const padrao = /^\d{3}\.\d{3}\.\d{3}-\d{2}$/;
+    return padrao.test(cpf);
+  }
+  
+  function validarTelefone(telefone) {
+    // Expressão regular para validar número de telefone
+    const padrao = /^\(\d{2}\)\s\d{4,5}-\d{4}$/;
+    return padrao.test(telefone);
+  }
+  
+  // Testando os validadores
+  const emailValido = validarEmail('exemplo@email.com');
+  const cpfValido = validarCPF('123.456.789-10');
+  const telefoneValido = validarTelefone('(99) 99999-9999');
+  
+  if (emailValido) {
+    console.log('Email válido!');
+  } else {
+    console.log('Email inválido1')
+  }
+  
+
+
